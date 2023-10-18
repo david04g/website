73d0c500574f978f6986079352eb890b8cf472d2
david04g <85910051+david04g@users.noreply.github.com>
2023-10-17 19:24:09 -0700

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-st.set_page_config(page_title="My Webpage", page_icon=":smiling_face_with_horns:",layout="wide")
-
-# header section
-st.subheader("Hi, I am David")
-st.title("A Sophmore Undergrad from UC Santa Cruz majoring in Computer Science B.S.")
-st.write("I am addicted to learning and improving")
